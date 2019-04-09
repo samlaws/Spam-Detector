e48d1c97eafb506c69d206c4af699a435fb07521
Samuel <Samuel.Laws@protonmail.com>
2019-04-09 14:14:47 +0100

    pickled the model

--- app.py
+++ app.py
@@ -1,42 +1,26 @@
 from flask import Flask,render_template,url_for,request
-import pandas as pd
 import numpy as np
 import pickle
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
 from sklearn.externals import joblib
 
+from model import score
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html', name='homepage')
+	my_score = score
+	return render_template('home.html', name='homepage', score=my_score)
 
 @app.route('/predict',methods=['POST'])
 def predict():
-	df= pd.read_csv("spam.csv", encoding="latin-1")
-	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
-	# Features and Labels
-	df['label'] = df['class'].map({'ham': 0, 'spam': 1})
-	X = df['message']
-	y = df['label']
+	NB_spam_model = open('NB_spam_model.pkl','rb')
+	spam_count_vectorizer = open('spam_count_vectorizer.pkl','rb')
 
-	# Extract Feature With CountVectorizer
-	cv = CountVectorizer()
-	X = cv.fit_transform(X) # Fit the Data
-	from sklearn.model_selection import train_test_split
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-	#Naive Bayes Classifier
-	from sklearn.naive_bayes import MultinomialNB
+	clf = joblib.load(NB_spam_model)
+	cv = joblib.load(spam_count_vectorizer)
 
-	clf = MultinomialNB()
-	clf.fit(X_train,y_train)
-	clf.score(X_test,y_test)
-	#Alternative Usage of Saved Model
-	# joblib.dump(clf, 'NB_spam_model.pkl')
-	# NB_spam_model = open('NB_spam_model.pkl','rb')
-	# clf = joblib.load(NB_spam_model)
 
 	if request.method == 'POST':
 		message = request.form['message']
